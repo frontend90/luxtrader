e60040af9335c1d15109aba151191e70824bbaf9
Vasiliy Pupkin <vpupkin@company.com>
2020-10-02 12:11:06 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -59,3 +59,29 @@ document.documentElement.addEventListener("click", function (e) {
     user_menu.classList.remove('show');
   }
 });
+
+/****************  время обратного отчета   ************/
+timeend = new Date();
+// IE и FF по разному отрабатывают getYear()
+timeend = new Date(timeend.getYear() > 1900 ? (timeend.getYear() + 1) : (timeend.getYear() + 1901), 0, 1);
+// для задания обратного отсчета до определенной даты укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ);
+// Для задания даты с точностью до времени укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ, ЧАСЫ-1, МИНУТЫ);
+function time() {
+  today = new Date();
+  today = Math.floor((timeend - today) / 1000);
+  tsec = today % 60;
+  today = Math.floor(today / 60);
+  if (tsec < 10) tsec = '0' + tsec;
+  tmin = today % 60;
+  today = Math.floor(today / 60);
+  if (tmin < 10) tmin = '0' + tmin;
+  thour = today % 24;
+  today = Math.floor(today / 24);
+  timestr = today + ":" + thour + ":" + tmin + ":" + tsec + "";
+  document.getElementById('time1').innerHTML = timestr;
+  document.getElementById('time2').innerHTML = timestr;
+  document.getElementById('time3').innerHTML = timestr;
+  window.setTimeout("time()", 1000);
+}
